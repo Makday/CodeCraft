bf25ec2d4478f73e540c4434c907c3963d311150
Makday <58140558+Makday@users.noreply.github.com>
2026-03-11 02:26:12 +0200

    add location attributes to the token class

--- src/main/java/com/minecraftdsl/Token.java
+++ src/main/java/com/minecraftdsl/Token.java
@@ -3,15 +3,21 @@ package com.minecraftdsl;
 public class Token {
     public final TokenType type;
     public final String literal;
+    public final int line;
+    public final int col;
 
-    public Token(TokenType type, String literal) {
+    public Token(TokenType type, String literal, int line, int col) {
         this.type = type;
         this.literal = literal;
+        this.line = line;
+        this.col = col;
     }
 
-    public Token(TokenType type) {
+    public Token(TokenType type, int line, int col) {
         this.type = type;
         this.literal = null;
+        this.line = line;
+        this.col = col;
     }
 
     @Override
