d81dea33b6d1deacce28ec2dea43c2244b251d42
Makday <58140558+Makday@users.noreply.github.com>
2026-03-28 01:20:19 +0200

    add mod entrypoint

---
+++ src/main/java/com/utm/elsd/codecraft/CodeCraft.java
@@ -0,0 +1,11 @@
+package com.utm.elsd.codecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CodeCraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("CodeCraft loaded!");
+    }
+}
