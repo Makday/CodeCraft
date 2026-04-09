8be1686308fa1e26852f69021d290fdc2f48fcf2
gabrielabitca <gabrielabitca2005@gmail.com>
2026-04-10 02:42:15 +0300

    Add /testinventory drop command for testing drop_item

--- src/main/java/com/utm/elsd/codecraft/CodeCraftClient.java
+++ src/main/java/com/utm/elsd/codecraft/CodeCraftClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.text.Text;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 
 public class CodeCraftClient implements ClientModInitializer {
     // game action executor for testing DSL actions
@@ -18,12 +19,30 @@ public class CodeCraftClient implements ClientModInitializer {
             dispatcher.register(ClientCommandManager.literal("testmove")
                     .then(ClientCommandManager.literal("forward")
                             .executes(context -> {
-                                var result = actionExecutor.executeAction("move_forward",1);
+                                var result = actionExecutor.executeAction("move_forward", 1);
                                 context.getSource().sendFeedback(
                                         Text.literal("move_forward(1): " + result.toString())
                                 );
                                 return 1;
                             })
+                    )
+            );
+
+            dispatcher.register(ClientCommandManager.literal("testinventory")
+                    .then(ClientCommandManager.literal("drop")
+                            .then(ClientCommandManager.argument("row", IntegerArgumentType.integer(0, 3))
+                                    .then(ClientCommandManager.argument("col", IntegerArgumentType.integer(0, 8))
+                                            .executes(context -> {
+                                                int row = IntegerArgumentType.getInteger(context, "row");
+                                                int col = IntegerArgumentType.getInteger(context, "col");
+                                                var result = actionExecutor.executeAction("drop_item", row, col);
+                                                context.getSource().sendFeedback(
+                                                        Text.literal("drop_item(" + row + ", " + col + "): " + result.toString())
+                                                );
+                                                return 1;
+                                            })
+                                    )
+                            )
                     )
             );
         });
